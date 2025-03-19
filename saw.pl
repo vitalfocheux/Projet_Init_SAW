3e1d9203344d56793a9be45841ae570dcf6d521e
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2025-03-19 11:00:17 +0100

    Prédicat d'equilibrage de sud, nord, est, ouest

--- saw.pl
+++ saw.pl
@@ -21,3 +21,18 @@ next(nord, est).
 dir_seq([], 0).
 dir_seq([X|L], N) :- N > 0, Nm1 is N-1, dir(X), dir_seq(L, Nm1).
 
+/* Prédicat qui permet de déterminer si une liste contient le même nombre de sud que de nord ou d'est que d'ouest */
+equilibre(L) :-
+    count(sud, L, SudCount),
+    count(nord, L, NordCount),
+    count(est, L, EstCount),
+    count(ouest, L, OuestCount),
+    (SudCount =\= NordCount; EstCount =\= OuestCount), !.
+
+/* Prédicat pour compter les occurrences d'un élément dans une liste */
+count(_, [], 0).
+count(X, [X|T], N) :-
+    count(X, T, N1),
+    N is N1 + 1, !.
+count(X, [_|T], N) :-
+    count(X, T, N).
